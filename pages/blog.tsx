0471efbf103977a0f8d7f3fa985dd1364ad62f98
bpkcongli <andrian8367@gmail.com>
2022-03-10 14:51:47 +0700

    testing server side rendering

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,119 +1,39 @@
-import Image from 'next/image';
 import LayoutPage from '../components/LayoutPage';
 
-export default function Blog() {
+interface Note {
+  id: string,
+  content: string,
+  date: string,
+  important: boolean,
+  userId: string,
+}
+
+interface BlogProps {
+  notes: Note[],
+}
+
+export default function Blog(props: BlogProps) {
+  const { notes } = props;
+
   return (
     <LayoutPage pageTitle="Blog">
       <div>
         <h1>Blogs</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <Image src="/profile.jpg" alt="profile" width={192} height={120} />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque
-          risus sit amet risus vehicula rhoncus. Vivamus viverra libero sed lacus accumsan
-          consequat. Fusce suscipit ante magna, ac iaculis urna vulputate in. Aenean ut
-          nibh et quam mattis laoreet. Vestibulum a elit ante. Duis sit amet justo nec ligula
-          auctor rutrum id vel tellus. Vestibulum vehicula suscipit dui, ut consequat leo
-          finibus sed.
-        </p>
-        <Image src="/akira.png" alt="akira" width={384} height={216} />
+        <ul>
+          {notes.map(({ id, content, date }) => <li key={id}>{content} - {date}</li>)}
+        </ul>
       </div>
     </LayoutPage>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch('https://fullstackopen-notes-apps.herokuapp.com/api/notes');
+  const { notes } = await res.json();
+
+  return {
+    props: {
+      notes,
+    },
+  };
+};
